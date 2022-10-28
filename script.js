057767c40c42f710b52b2513358a2ee9253eeaef
Rizal Mila Pambudi <107825914+Rijal25@users.noreply.github.com>
2022-10-28 21:34:33 +0700

    script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function main () {
+    let nomor = document.getElementById("nomor").value
+
+    confirm("Apakah yakin dengan inputan Anda?")
+
+    nomor == 2 ? alert("Angka yang anda masukkan benar adalah 2") : alert(`Angka yang anda masukkan salah adalah ${nomor}`)
+
+}
